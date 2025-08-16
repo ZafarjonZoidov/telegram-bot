8a11e4066b8b84dc252509ba2ac9b4f26ab69558
smedleya <smedleya@student.21-school.ru>
2025-08-16 13:00:37 +0500

    t

--- bot.php
+++ bot.php
@@ -1,22 +1,15 @@
 <?php
-$input = file_get_contents("php://input");
-file_put_contents("log.txt", $input . PHP_EOL, FILE_APPEND);
+$update = json_decode(file_get_contents("php://input"), true);
 
-$update = json_decode($input, true);
+if (isset($update["message"])) {
+    $chat_id = $update["message"]["chat"]["id"];
+    $text = $update["message"]["text"];
 
-if (isset($update['message'])) {
-    $chat_id = $update['message']['chat']['id'];
-    $text = $update['message']['text'] ?? '';
+    $token = "8406794244:AAEJ6RoKkEH40F0XTKMdNZA9SGz3pmZehXU";
 
-    $token = "BOT_TOKEN"; // Tokeningizni shu yerga yozing
     $url = "https://api.telegram.org/bot$token/sendMessage";
 
-    $data = [
-        "chat_id" => $chat_id,
-        "text" => "Siz yubordingiz: $text"
-    ];
-
-    file_get_contents($url . "?" . http_build_query($data));
+    file_get_contents($url . "?chat_id=" . $chat_id . "&text=Salom! Siz yozdingiz: " . urlencode($text));
 }
 
 http_response_code(200);
